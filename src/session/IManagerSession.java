583b03563e70cae0b380d6d6d3d46b0cfd7eaeec
Charles <Charles@Laptop-Charles>
2018-10-30 15:21:52 +0100

    test 123

--- src/session/IManagerSession.java
+++ src/session/IManagerSession.java
@@ -1,5 +1,9 @@
 package session;
 
+import java.util.Set;
+
 public interface IManagerSession {
+	
+	public Set<String> getBestClients();
 
 }
--- src/session/ManagerSession.java
+++ src/session/ManagerSession.java
@@ -1,9 +1,17 @@
 package session;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class ManagerSession implements IManagerSession {
 
 	public ManagerSession()  {
 		// TODO Auto-generated constructor stub
 	}
+	
+	public Set<String> getBestClients(IManagerSession ms) throws Exception {
+		return new HashSet<String>();
+	}
+
 
 }
